f14a9f57d53be711a3d35a9c8b3c59f30e2fce8c
andynguyen30 <92894177+andynguyen30@users.noreply.github.com>
2024-04-13 19:59:01 -0700

    Update TicTacToeClient.py

--- TicTacToeClient.py
+++ TicTacToeClient.py
@@ -16,13 +16,15 @@ client.connect(ADDR)
 
 class Game:
     # class for tic-tac-toe game
-    def __init__(self):
-        self.positions = [x for x in range(1, 10)]
+   
     
     # sets the client's symbol to X or O, received from server
     def set_symbol(self, symbol):
         self.symbol = symbol
-    
+        
+     def __init__(self):
+        self.positions = [x for x in range(1, 10)]
+
     # sets who goes first, received from server
     def set_turn(self, turn):
         if turn == '0':
@@ -35,8 +37,26 @@ class Game:
         if self.turn == True:
             self.show_board()
             self.get_user_input()
-
-    # receive message from server from other client, update board with their position
+            
+    # pass the spot you chose to the server to send to the other client
+    def pass_user_input(self, spot):
+        send(spot)
+        self.turn = False
+    
+    # check to see if client is the winner
+    def check_winner(self, spot):
+        p = self.positions
+        for i in ['X', 'O']:
+            if (p[0] == i and ((p [1] == i and p[2] == i) or \
+                (p[3] == i and p[6] == i) or (p[4] == i and p[8] == i))) or \
+                    (p[8] == i and ((p[7] == i and p[6] == i) or \
+                        (p[5] == i and p[2] == i))) or (p[2] == i and p[4] == i and p[6] == i) or \
+                            (p[1] == i and p[4] == i and p[7] == i) or \
+                                (p[3] == i and p[4] == i and p[5] == i):
+                send(f'L{spot}')
+                self.end_win()
+                                    
+    # receive message from server from the other client, update the board with their position
     def receive_user_input(self, spot):
         if spot[0] in {'1', '2', '3', '4', '5', '6', '7', '8', '9'}:
             if self.symbol == 'X':
@@ -56,25 +76,15 @@ class Game:
             if spot[0] == 'T':
                 self.end_tie()
 
-    # pass the spot you chose to the server to send to the other client
-    def pass_user_input(self, spot):
-        send(spot)
-        self.turn = False
-    
-    # check to see if you won
-    def check_winner(self, spot):
-        p = self.positions
-        for i in ['X', 'O']:
-            if (p[0] == i and ((p [1] == i and p[2] == i) or \
-                (p[3] == i and p[6] == i) or (p[4] == i and p[8] == i))) or \
-                    (p[8] == i and ((p[7] == i and p[6] == i) or \
-                        (p[5] == i and p[2] == i))) or (p[2] == i and p[4] == i and p[6] == i) or \
-                            (p[1] == i and p[4] == i and p[7] == i) or \
-                                (p[3] == i and p[4] == i and p[5] == i):
-                send(f'L{spot}')
-                self.end_win()
-    
-    # check to see if it was a tie
+    # prints the current board
+    def show_board(self):
+        print(f'\n {self.positions[0]} | {self.positions[1]} | {self.positions[2]} ')
+        print('---+---+---')
+        print(f' {self.positions[3]} | {self.positions[4]} | {self.positions[5]} ')
+        print('---+---+---')
+        print(f' {self.positions[6]} | {self.positions[7]} | {self.positions[8]} \n')
+        
+    # check to see if it is a tie
     def check_tie(self, spot):
         count = 0
         for i in self.positions:
@@ -85,16 +95,8 @@ class Game:
         if count == 9:
             send(f'T{spot}')
             self.end_tie()
-
-    # prints the current board
-    def show_board(self):
-        print(f'\n {self.positions[0]} | {self.positions[1]} | {self.positions[2]} ')
-        print('---+---+---')
-        print(f' {self.positions[3]} | {self.positions[4]} | {self.positions[5]} ')
-        print('---+---+---')
-        print(f' {self.positions[6]} | {self.positions[7]} | {self.positions[8]} \n')
-
-    # updates the board based on user input, or message received from other client
+            
+    # updates the board based on the user input, or message received from other client
     def update_board(self, spot, opponent_symbol=None):
         if opponent_symbol is not None:
             self.positions[spot] = opponent_symbol
@@ -115,37 +117,33 @@ class Game:
                     finished = True
             except:
                 print('Not a valid number!')
+        #check for winner
         self.check_winner(str(spot))
+        #check if tie
         self.check_tie(str(spot))
         self.pass_user_input(str(spot))
     
-    # function that displays end message if there is a winner/loser
-    def end_win(self, msg=None):
-        if msg is not None:
-            print(f'\n{msg}')
-        else:
-            print('You win!')
-        send(DISCONNECT_MESSAGE)
-        quit()
+    
     
     # function that displays end message if it was a tie
     def end_tie(self):
         print('\nIt was a tie!')
         send(DISCONNECT_MESSAGE)
         quit()
-
+        
+    # function that displays end message if there is a winner/loser
+    def end_win(self, msg=None):
+        if msg is not None:
+            print(f'\n{msg}')
+        else:
+            #print out You win for the winner
+            print('You win!')
+        send(DISCONNECT_MESSAGE)
+        quit()
 # starts the game
 def start_game(game):
     game.run_game()
 
-# sends the location and win/lose/tie condition if applicable after client enters input
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
 
 # receives input from other client through server, sends to game's receive_user_input function
 def receive():
@@ -164,7 +162,14 @@ def receive():
                 start_game(game)
                 count += 1
                 
-
+# sends the location and win/lose/tie condition if applicable after client enters input
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
 
 # create and start thread
 t = threading.Thread(target=receive)
